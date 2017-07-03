f1ff23dfc1381f842e01256f8a8eb070dfc10cfc
d <damlaakaraca@gmail.com>
2017-07-03 15:39:48 +0300

    WebView uygulama, url ve html

--- WebViewUygulama/src/com/wvu/webviewuygulama/Html.java
+++ WebViewUygulama/src/com/wvu/webviewuygulama/Html.java
@@ -9,7 +9,7 @@ public class Html extends Activity {
 	@Override 
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.html);
+		setContentView(R.layout.html); 
 
 		WebView html = (WebView) findViewById(R.id.wv2);
 		String htmlSayfa = "<html><body><h1>Html Sayfasý</h1><h2>Sayfa detayý</h2></body></html>";
